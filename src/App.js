f5b7c718f99d97473e781a033b7eaa6a22fba409
Daniel <danieruone@gmail.com>
2021-06-25 20:29:09 -0500

    socket connection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
+import { useState, useEffect } from "react";
 import { BandList } from "components/BandList";
+import { io } from "socket.io-client";
+
+const connectSocketServer = () => {
+  const socket = io("http://localhost:8080", {
+    transports: ["websocket"],
+  });
+  return socket;
+};
 
 function App() {
-  return <BandList />;
+  const [socket, setSocket] = useState(connectSocketServer());
+  const [online, setOnline] = useState(false);
+  const [bandList, setBandList] = useState([]);
+
+  useEffect(() => {
+    setOnline(true);
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("connect", (data) => {
+      console.log(data);
+      setOnline(true);
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("disconnect", (data) => {
+      console.log(data);
+      setOnline(false);
+    });
+  }, [socket]);
+
+  useEffect(() => {
+    socket.on("current-bands", (bands) => {
+      setBandList(bands);
+    });
+  }, [socket]);
+
+  return (
+    <div>
+      {online ? (
+        <h1 style={{ color: "green" }}>Connected</h1>
+      ) : (
+        <h1 style={{ color: "red" }}>Disconnected</h1>
+      )}
+      <BandList bandList={bandList} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/BandList/index.js
+++ src/components/BandList/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Band } from "components/Band";
 
-export const BandList = () => {
-  return <Band />;
+export const BandList = ({ bandList }) => {
+  return (
+    <div>
+      {bandList.map((band, idx) => {
+        return <Band key={idx} {...band} />;
+      })}
+    </div>
+  );
 };
